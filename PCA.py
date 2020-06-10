99e3df457e2cfee2e9753237eb32f336b35651be
littlelittlemoon <littlemeemoon@gmail.com>
2020-06-10 16:27:48 +0800

    change private variables to public variables of PCA Class

--- PCA.py
+++ PCA.py
@@ -3,15 +3,15 @@ from Projection import Projection
 class PCA(Projection):
      def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.__X_mean = None
-        self.__eigenvalues = None
+        self.X_mean = None
+        self.eigenvalues = None
 
     def fit(self, X, y=None):
         assert X.ndim == 2, 'X can only be a 2-d matrix'
 
         # Center the data
-        self.__X_mean = np.mean(X, axis=0)
-        X = X - self.__X_mean
+        self.X_mean = np.mean(X, axis=0)
+        X = X - self.X_mean
         n, d = X.shape
 
         # If the d >> n then use dual PCA for efficiency
@@ -29,16 +29,16 @@ class PCA(Projection):
         if use_dual_pca:
             U = X.dot(U).dot(np.diag(1 / np.sqrt(S * (n - 1))))
 
-        self.__subspace = U
-        self.__eigenvalues = S
+        self.subspace = U
+        self.eigenvalues = S
 
         return self
 
     def project(self, X):
-        self.__check_fitted()
-        X = X - self.__X_mean
+        self.check_fitted()
+        X = X - self.X_mean
         return np.dot(X, self.pro_subspace)
 
     def reconstruct(self, X):
-        self._check_fitted()
-        return np.dot(X, self.pro_subspace.T) + self.__X_mean
+        self.check_fitted()
+        return np.dot(X, self.pro_subspace.T) + self.X_mean
